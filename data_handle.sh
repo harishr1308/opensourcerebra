c4eb94fe8d9df152ad14117ef3e97b2b5486d383
Harish Ranganathan <harishr1308@users.noreply.github.com>
2016-04-29 13:39:32 +0530

    Update data_handle.sh

--- data_handle.sh
+++ data_handle.sh
@@ -13,8 +13,51 @@ cd ..
 mkdir tno
 mkdir html
 cd html
-g++ tnosplit.cpp
+g++ tnosplit.cpp #if your file contains multiple transcripts in it
 ./a.out
+#python tnodownload.py #if you need to download transcripts uncomment this line and make changes to the URL, username and password accordingly
+g++ tnolisting.cpp
+./a.out
+g++ tnolistsort.cpp
+./a.out
+g++ tnoaudio.cpp
+./a.out
+g++ tag.cpp
+./a.out
+g++ sfile.cpp
+./a.out
+g++ scheck.cpp
+./a.out
+g++ scheck_blank.cpp
+./a.out
+g++ tnosfilelisting.cpp
+./a.out
+g++ tnoscfilelistsort.cpp
+./a.out
+g++ tnosefilelistsort.cpp
+./a.out
+g++ tnoffilelisting.cpp
+./a.out
+g++ findingmissingfiles.cpp
+./a.out
+g++ tnofcfilelistsort.cpp
+./a.out
+g++ tnofefilelistsort.cpp
+./a.out
+g++ missingfilessort.cpp
+./a.out
+g++ missingfcfilessorting.cpp
+./a.out
+g++ missingfefilessorting.cpp
+./a.out
+g++ missingscfilessorting.cpp
+./a.out
+g++ missingsefilessorting.cpp
+./a.out
+g++ missingtnofilessorting.cpp
+./a.out
+chmod +x MoveTNo.sh
+./MoveTNo.sh
 
 chmod +x model_gen.sh
 ./model_gen.sh
