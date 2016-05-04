1885af7325da988855d59c912d0f78c2bad82691
Harish Ranganathan <harishr1308@users.noreply.github.com>
2016-05-05 00:12:35 +0530

    Added files via upload

---
+++ filecheck.py
@@ -0,0 +1,34 @@
+import language_check
+import glob   
+import os
+
+def replace_last(source_string, replace_what, replace_with):
+    head, sep, tail = source_string.rpartition(replace_what)
+    return head + replace_with + tail
+
+tool = language_check.LanguageTool('en-UK')
+path = './../files/ferror/*.txt'   
+files=glob.glob(path)
+ 
+for file in files:     
+	f1=open(file, 'r')  
+	filename = os.path.basename(file)
+	outputpath = './../files/flangcorr/'
+	filename = replace_last(filename,'e','l')
+	text = f1.read()
+	print filename
+	text.encode('utf-8')
+	#text.encode('\"', 'ignore')
+	matches = tool.check(text)
+	print len(matches)
+	# q = len(matches)
+	# for x in range(0,q):
+	# 	matches[x].fromy, matches[x].fromx
+	# 	print (matches[x])
+	# print (matches[1])
+	outputpath += filename
+	temp = language_check.correct(text,matches);
+	f2 = open(outputpath,'w')
+	f2.write(temp);
+	f2.close();
+	f1.close();
