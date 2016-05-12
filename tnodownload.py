34161607069d46bd873dac3c94a41fb1937cec35
Harish Ranganathan <harishr1308@users.noreply.github.com>
2016-05-12 18:22:44 +0530

    Update tnodownload.py

--- tnodownload.py
+++ tnodownload.py
@@ -1,15 +1,38 @@
 import requests
 from requests.auth import HTTPBasicAuth
-url = "URL"
-usr = 'Username'
-pas = 'Password'
+url = 'http://www.dictate.it/bosi/sitetools/LetterManager/tools/getLetterData'
+usr = "Cerebra_NM"
+pas = 'C3r$br@555'
+filecount = 1;
+filenocount = 0;
+name = ""
 with open('todownloadtno.txt') as fp:
- for line in fp:
-  name = line.rstrip('\n')
-  print name
-  key_value = {'instr':name}
-  r = requests.post(url, auth=HTTPBasicAuth(usr, pas), data=key_value)
-  fp = open(name+".txt","w")
-  fp.write(r.text.encode('utf8','ignore'))
-  fp.close()
-  print r.text
+	count = 0
+	for line in fp:
+		filenocount +=1
+		name += line.rstrip('\n')
+		print filenocount , line.rstrip('\n')
+		if count>50:
+			key_value = {'instr':name}
+			r = requests.post(url, auth=HTTPBasicAuth(usr, pas), data=key_value)
+			fp = open(str(filecount)+".txt","w")
+			fp.write(r.text.encode('utf8','ignore'))
+			fp.close()
+			count = 0
+			print filecount,"is created"
+			filecount+=1
+			name = ""
+		else:
+			count+=1
+			name+=','
+	if name!="":
+		key_value = {'instr':name}
+			r = requests.post(url, auth=HTTPBasicAuth(usr, pas), data=key_value)
+			fp = open(str(filecount)+".txt","w")
+			fp.write(r.text.encode('utf8','ignore'))
+			fp.close()
+			count = 0
+			print filecount,"is created"
+			filecount+=1
+			name = ""
+ # print r.text
